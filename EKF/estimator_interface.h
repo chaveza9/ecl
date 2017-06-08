5a6e302723b763de2e69c588ebd396629b3a1ee8
Paul Riseborough <p_riseborough@live.com.au>
2017-06-08 21:14:20 +1000

    EKF: Enable operation near magnetic poles

--- EKF/estimator_interface.h
+++ EKF/estimator_interface.h
@@ -183,7 +183,13 @@ public:
 	parameters *getParamHandle() {return &_params;}
 
 	// set vehicle landed status data
-	void set_in_air_status(bool in_air) {_control_status.flags.in_air = in_air;}
+	void set_in_air_status(bool in_air) {
+		// store the latest yaw angle if landing
+		if (!in_air && _control_status.flags.in_air) {
+			_last_inflight_yaw = getOutputYawAngle();
+		}
+		_control_status.flags.in_air = in_air;
+	}
 
 	// get vehicle landed status data
 	bool get_in_air_status() {return _control_status.flags.in_air;}
@@ -329,6 +335,9 @@ public:
 
 	static const unsigned FILTER_UPDATE_PERIOD_MS = 12;	// ekf prediction period in milliseconds - this should ideally be an integer multiple of the IMU time delta
 
+	// return Euler yaw angle  in radians at output time horizon using the best conditioned of a 321 or 312 sequence to avoid gimbal lock
+	float getOutputYawAngle(void);
+
 protected:
 
 	parameters _params;		// filter parameters
@@ -380,6 +389,7 @@ protected:
 	imuSample _imu_sample_new{};		// imu sample capturing the newest imu data
 	Matrix3f _R_to_earth_now;		// rotation matrix from body to earth frame at current time
 	Vector3f _vel_imu_rel_body_ned;		// velocity of IMU relative to body origin in NED earth frame
+	float _last_inflight_yaw{0.0f};		// value of output yaw angle saved at touchdown (rad)
 
 	uint64_t _imu_ticks{0};	// counter for imu updates
 
